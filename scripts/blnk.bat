b6a22a7bc95090294f8a6a2981865cd6b655af15
Poikilos <7557867+Poikilos@users.noreply.github.com>
2024-04-12 11:12:27 -0400

    Show error logs reliably. Fix more silent batch errors (cmd.exe bugs, lol--always neq if syntax error in that case).

--- scripts/blnk.bat
+++ scripts/blnk.bat
@@ -28,27 +28,44 @@ echo. >> "%logsDir%\out.log"
 
 REM Output out.log
 type "%logsDir%\out.log"
-
+REM echo code is %code%
 REM Check return code
+set err_line_count=0
+REM ^ Ensure it is never blank, or neq 0 will always run
+REM   "0 was unexpected" since nested "if" will resolve to:
+REM   "if  gtr 0 ("
+FOR /F %%i IN ('TYPE "%logsDir%\err.log" ^| FIND /C /V ""') DO SET err_line_count=%%i
 if %code% neq 0 (
+    echo "[blnk.bat] error nonzero return code %code%
     REM BAD Count lines in err.log
-    for /f %%A in ('find /v /c "" ^< "%logsDir%\err.log"') do set "err_line_count=%%A"
+    REM for /f %%A in ('find /v /c "" ^< "%logsDir%\err.log"') do set "err_line_count=%%A"
+    
+    REM NOTE: Using %% is only necessary in a batch file. To test it live, use only % not %%
+    REM   (See <https://stackoverflow.com/questions/9311562/a-was-unexpected-at-this-time>)
     echo err_line_count=%err_line_count%
     type "%logsDir%\err.log" 1>&2
     echo Showing a GUI dialog box... 1>&2
     if %err_line_count% gtr 0 (
         REM Show error message box
         REM mshta vbscript:Execute("msgbox ""See %logsDir%\out.log (usually err.log, but that appears to be blank)"":close")
+        notepad "%logsDir%\err.log"
         type "%logsDir%\err.log" 1>&2
     ) else (
         set "msg=Blnk had an unrecorded error. See %logsDir%\out.log for information about the last run."
         REM mshta vbscript:Execute("msgbox ""%msg%"":close")
+        echo. >> "%logsDir%\out.log"
+        echo "[blnk.bat] code %code%" >> "%logsDir%\out.log"
+        echo "[blnk.bat] err_line_count %err_line_count%" >> "%logsDir%\out.log"
+        echo "[blnk.bat] %msg%" >> "%logsDir%\out.log"
+        notepad "%logsDir%\out.log"
     )
     exit /b %code%
 ) else (
+    echo "[blnk.bat] OK return code %code%"
     REM OK Count lines in err.log
-    for /f %%A in ('find /v /c "" ^< "%logsDir%\err.log"') do set "err_line_count=%%A"
-    echo err_line_count=%err_line_count%
+    REM for /f %%A in ('find /v /c "" ^< "%logsDir%\err.log"') do set "err_line_count=%%A"
+    REM for /f %%A in ('TYPE "%logsDir%\err.log" ^| FIND /C /V ""') do set "err_line_count=%%A"
+    REM echo err_line_count=%err_line_count%
     if %err_line_count% gtr 0 (
         type "%logsDir%\err.log" 1>&2
     )
